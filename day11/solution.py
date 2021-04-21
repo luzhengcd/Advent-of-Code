6702a1702f19bea461307852f135985c0ada38d1
Zheng, Lu <lu.zheng@optum.com>
2021-04-21 12:48:39 -0400

    solution for day 11

--- day11/solution.py
+++ day11/solution.py
@@ -6,12 +6,13 @@ import time
 req1 = {'abc', 'bcd', 'cde', 'def', 'efg', 'fgh',
         'pqr', 'qrs', 'rst', 'stu', 'tuv', 'uvw', 'vwx', 'wxy', 'xyz'}
 
-req1_pattern = '|'.join([''.join(list(i)[::-1]) for i in req1])
+req1_pattern = '|'.join(req1)
 req2_exclude = {'i', 'o', 'l'}
 
 valid_letter = [i for i in string.ascii_lowercase if i not in req2_exclude]
 alpha_to_num = {letter: idx for idx, letter in enumerate(valid_letter)}
 
+# adding i, l, o in case the input has the three letters
 alpha_to_num['i'] = 7
 alpha_to_num['l'] = 9
 alpha_to_num['o'] = 11
@@ -22,7 +23,7 @@ def check_req1(s):
     return re.finditer(req1_pattern, s, overlapped=True)
     
 
-def check_req2(s):
+def check_req3(s):
     pattern = r'([a-z])\1'
     return len(set(re.findall(pattern, s)))
 
@@ -30,37 +31,38 @@ def check_pass(s):
     # check requirement 1
     iterator = check_req1(s)
     for _ in iterator:
-        # check requirement 2
-        if check_req2(s) >= 2:
+        # requirement 2 is met when generating the new password
+        # check requirement 3
+        if check_req3(s) >= 2:
             return True
     return False
 
-def get_next(pass_reversed, idx):
+def get_next(password_lst, idx):
     '''
-    password is a list of letters in the password in reversed order
+    password is a list of letters in the password
     '''
-    if idx == 8:
+    if idx == -1:
         print('The password has reached its upper bound')
         return
     else:  
         
-        curr = alpha_to_num[pass_reversed[idx]]
+        curr = alpha_to_num[password_lst[idx]]
         if curr == 22:
-            pass_reversed[idx] = 'a'
-            get_next(pass_reversed, idx + 1)
+            password_lst[idx] = 'a'
+            get_next(password_lst, idx - 1)
         else:
-            pass_reversed[idx] = num_to_alpha[curr + 1]
+            password_lst[idx] = num_to_alpha[curr + 1]
             return
 
 def main(password):
     p = False
-    pass_lst = list(password)[::-1]
+    pass_lst = list(password)
     while not p:
-        get_next(pass_lst, 0)
-        pass_reverse = ''.join(pass_lst) 
-        p = check_pass(pass_reverse)
+        get_next(pass_lst, 7)
+        password = ''.join(pass_lst) 
+        p = check_pass(password)
         
-    return pass_lst
+    return ''.join(pass_lst)
 
 if __name__ == '__main__':
     # password = 'vzbxxyzz'
@@ -75,12 +77,12 @@ if __name__ == '__main__':
             break
 
     print('input is ', password)
-    ans1_reverse = main(password)
-    ans1 = ''.join(ans1_reverse[::-1]) 
-    print('Answer for part1: ', ans1)
-    ans2_reverse = main(ans1)
-    ans2 = ''.join(ans2_reverse[::-1])  
-    print('Answer for part2: ', ans2) 
+    ans1 = main(password)
+    ans1_str = ''.join(ans1) 
+    print('Answer for part1: ', ans1_str)
+    ans2 = main(ans1_str)
+    ans2_str = ''.join(ans2)  
+    print('Answer for part2: ', ans2_str) 
     end = time.time()
 
     print('time elapsed: ', end - start)
