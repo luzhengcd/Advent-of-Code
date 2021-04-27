8241dbee99a84c858daf17495b276afa293a6179
luzheng <lu.zhengcd@hotmail.com>
2021-04-26 22:50:14 -0400

    solution for day12

--- day12/solution.py
+++ day12/solution.py
@@ -1,13 +1,15 @@
 import json
+import time    
+import sys 
 
 def read_input(path):
     with open(path) as f:
         return json.load(f)
 
 
-SUM = 0
+SUM_PART1 = 0
 def solve_part1(data):
-    global SUM
+    global SUM_PART1
     if isinstance(data, dict):
         for key, val in data.items():
             solve_part1(key)
@@ -16,41 +18,44 @@ def solve_part1(data):
         for i in data:
              solve_part1(i)
     elif isinstance(data, (int, float)):
-        SUM += data
+        SUM_PART1 += data
 
     return
 
-# SUM_PART2 = 0
-def solve_part2(data, local_sum, d):
-    global SUM_PART2
-        
-    if data == 'red' and d == 1:
-        return False, local_sum
-
-    if isinstance(data, dict):
-        local_sum = 0
-        for key, val in data.items():
-            has_red1, updated_local_sum = solve_part2(key, local_sum, d + 1)
-            has_red2, updated_local_sum = solve_part2(val, local_sum, d + 1)
-            print(key, val)
-            print(has_red1, has_red2)
-            if not has_red1 and not has_red2:
-                continue
-            else:
-                SUM_PART2 -= updated_local_sum
-                break
-    elif isinstance(data, list):
-        for i in data:
-            _, _ = solve_part2(i, 0, d + 1)
-    elif isinstance(data, (int, float)):
-        print('test lst')
-        local_sum += data
-        SUM_PART2 += data
-
-    return  True, local_sum
+def has_red(data):    
+    for key, val in data.items():
+        if key == 'red' or val == 'red':
+            return True
+    return False
 
 SUM_PART2 = 0
-data = {"d":"d","e":[1,2,3,4],"f":5}
-# data = read_input('input.txt')
-solve_part2(data, 0, 0)
-print(SUM_PART2)
+def solve_part2(data):
+    global SUM_PART2
+    if isinstance(data, dict):
+        if has_red(data):
+            return
+        for key, val in data.items():
+            solve_part2(key)
+            solve_part2(val)
+    elif isinstance(data, list):
+        for i in data:
+             solve_part2(i)
+    elif isinstance(data, (int, float)):
+        SUM_PART2 += data
+    return           
+
+def main(path = 'input.txt'):
+
+    data = read_input(path)
+    solve_part1(data)
+    solve_part2(data)
+
+    print('Answer for part 1: ', SUM_PART1)
+    print('Answer for part 2: ', SUM_PART2)
+
+if __name__ == '__main__':
+    start = time.time()
+    data_path = sys.argv[1]
+    main(data_path)
+    end = time.time()
+    print('Time elapsed: ', end - start)
