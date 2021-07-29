7e6be92aeb2d2e83421d040dde2ab2c9ac20a4f2
Zheng, Lu <lu.zheng@optum.com>
2021-07-29 09:50:35 -0400

    day 19 part1 update var naming

--- day19/solution.py
+++ day19/solution.py
@@ -10,34 +10,37 @@ def parse(path = 'input.txt'):
 				from_char, to_char = tokens[0], tokens[-1]
 				replacements[from_char].append(to_char)
 			else:
-				start = line.strip()
-	return replacements, start
+				molecule = line.strip()
+	return replacements, molecule
 
-def parse_start(start_str):
-	start_lst = []
+def parse_molecule(molecule_str):
+	element_lst = []
 	upper_letters = set(string.ascii_uppercase)
 	prev = None
-	for s in start_str:
-		if s in upper_letters:
+	for s in molecule_str:
+		if s in upper_letters or s == 'e':
 			if prev is not None:
-				start_lst.append(prev)
+				element_lst.append(prev)
 			prev = s
+			if s == 'e':
+				element_lst.append(s)
+				prev = None
 		else:
-			start_lst.append(prev + s)
+			element_lst.append(prev + s)
 			prev = None
 	if prev is not None:
-		start_lst.append(prev)
-	return start_lst
+		element_lst.append(prev)
+	return element_lst
 		
 
 def main():
-	replacements, start = parse()
-	module_set = set()
-	start_lst = parse_start(start)
-	for idx, s in enumerate(start_lst):
+	replacements, molecule = parse()
+	molecule_set = set()
+	element_lst = parse_molecule(molecule)
+	for idx, s in enumerate(element_lst):
 		for replacement in replacements[s]:
-			module_set.add( ''.join(start_lst[:idx] + [replacement] + start_lst[idx + 1 : ]))
-	print('Answer for part 1: {}'.format(len(module_set)))
+			molecule_set.add( ''.join(element_lst[:idx] + [replacement] + element_lst[idx + 1 : ]))
+	print('Answer for part 1: {}'.format(len(molecule_set)))
 
 if __name__ == '__main__':
 	main()
