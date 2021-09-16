3e1daea153875de4b1fbca1ae4db381de1b941f3
Zheng, Lu <lu.zheng@optum.com>
2021-09-16 11:45:24 -0400

    solution for both parts

--- day23/solution.py
+++ day23/solution.py
@@ -17,67 +17,69 @@ class Register:
     def jie(self, seq_no, jmp_step):
         if self.val % 2 == 0:
             return self.jmp(seq_no, jmp_step)
-        return seq_no
+        return seq_no + 1
     
     def jio(self, seq_no, jmp_step):
         if self.val == 1:
             return self.jmp(seq_no, jmp_step)
-        return seq_no
+        return seq_no + 1
 
-A = Register(0)
-B = Register(0)
 
 def parse_input(path = 'input.txt'):
     with open(path) as f:
         lines = f.readlines()
     return {key: val.strip() for key, val in enumerate(lines)}
 
-def solve(instruction_seq, seq_no):
-    if seq_no >= len(instruction_seq):
-        return
-    curr_instruction_line = instruction_seq[seq_no]
-    
-    instruction_split = curr_instruction_line.split()
+def solve(A, B, instruction_seq, seq_no):
+    length = len(instruction_seq)
 
-    instruction = instruction_split[0]
-    register = instruction_split[1].strip(',')
-    if instruction == 'hlf':
-        if register == 'a':
-            A.hlf()
-        else:
-            B.hlf()
-        solve(instruction_seq, seq_no + 1)
-    elif instruction == 'tpl':
-        if register == 'a':
-            A.tpl()
-        else:
-            B.tpl()
-        solve(instruction_seq, seq_no + 1)
-    elif instruction == 'inc':
-        if register == 'a':
-            A.inc()
-        else:
-            B.inc() 
-        solve(instruction_seq, seq_no + 1) 
-    elif instruction == 'jmp':
-        step = instruction_split[-1] 
-        solve(instruction_seq, seq_no + step) 
-    elif instruction == 'jio':
-        step = int(instruction_split[-1])
-        if register == 'a':
-            solve(instruction_seq, A.jio(seq_no, step))
-        else:
-            solve(instruction_seq, B.jio(seq_no, step))
-    elif instruction == 'jie':
-        step = int(instruction_split[-1])
-        if register == 'a':
-            solve(instruction_seq, A.jie(seq_no, step))
-        else:
-            solve(instruction_seq, B.jie(seq_no, step))
-    else:
-        return 
+    while seq_no < length:
+        curr_instruction_line = instruction_seq[seq_no]
+        instruction_split = curr_instruction_line.split()
+
+        instruction = instruction_split[0]
+        register = instruction_split[1].strip(',')
+        if instruction == 'hlf':
+            if register == 'a':
+                A.hlf()
+            else:
+                B.hlf()
+            seq_no += 1
+        elif instruction == 'tpl':
+            if register == 'a':
+                A.tpl()
+            else:
+                B.tpl()
+            seq_no += 1
+        elif instruction == 'inc':
+            if register == 'a':
+                A.inc()
+            else:
+                B.inc()
+            seq_no += 1 
+        elif instruction == 'jmp':
+            step = int(instruction_split[-1])
+            seq_no += step
+        elif instruction == 'jio':
+            step = int(instruction_split[-1])
+            if register == 'a':
+                seq_no = A.jio(seq_no, step)
+            else:
+                seq_no = B.jio(seq_no, step)
+        elif instruction == 'jie':
+            step = int(instruction_split[-1])
+            if register == 'a':
+                seq_no = A.jie(seq_no, step)
+            else:
+                seq_no = B.jie(seq_no, step)
+    return B.val
 
 if __name__ == '__main__':
-   instruction_seq = parse_input()
-   solve(instruction_seq, 0)
-   print(A.val, B.val) 
+    instruction_seq = parse_input()
+    A = Register(0)
+    B = Register(0) 
+    print('Answer for part1: ', solve(A, B, instruction_seq, 0))
+
+    A = Register(1)
+    B = Register(0) 
+    print('Answer for part1: ', solve(A, B, instruction_seq, 0))
